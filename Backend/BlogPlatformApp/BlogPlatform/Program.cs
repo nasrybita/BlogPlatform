a09dbc3be1da0f052685c0228ff50b0d25fb8ac5
bita <nasrybita@gmail.com>
2025-10-20 17:13:01 +0330

    Configure EF Core and FluentValidation in Program.cs

--- Backend/BlogPlatformApp/BlogPlatform/Program.cs
+++ Backend/BlogPlatformApp/BlogPlatform/Program.cs
@@ -1,11 +1,32 @@
+﻿using BlogPlatform.Data;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+//...
+// Add DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//...
+// Add Controllers + FluentValidation
 builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
