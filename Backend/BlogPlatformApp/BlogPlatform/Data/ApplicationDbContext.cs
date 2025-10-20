6fa9b4a370af679ed7db3c95dba5106ff75615ab
bita <nasrybita@gmail.com>
2025-10-20 17:18:18 +0330

    Add ApplicationDbContext class and configure DbSets

---
+++ Backend/BlogPlatformApp/BlogPlatform/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BlogPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        // DbSets
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<View> Views { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+
+
+    }
+
+}
