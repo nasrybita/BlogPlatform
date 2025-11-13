561ebd20a7629b76e4d566d825d9eedd104c8694
bita <nasrybita@gmail.com>
2025-11-13 19:30:13 +0330

    Add migration: Add view count to Post

---
+++ Backend/BlogPlatformApp/BlogPlatform/Migrations/20251108164753_AddViewCountToPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddViewCountToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ViewCount",
+                table: "Posts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ViewCount",
+                table: "Posts");
+        }
+    }
+}
