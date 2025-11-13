6bfaf073fedbff5a4094ec109cd44039f16653c5
bita <nasrybita@gmail.com>
2025-11-13 19:45:04 +0330

    Add new Validators folder with validation classes

---
+++ Backend/BlogPlatformApp/BlogPlatform/Validators/Post/PostUpdateDtoValidator.cs
@@ -0,0 +1,63 @@
+﻿using BlogPlatform.DTOs.Requests;
+using BlogPlatform.Enums;
+using FluentValidation;
+
+namespace BlogPlatform.Validators.Post
+{
+    public class PostUpdateDtoValidator : AbstractValidator<PostUpdateDto>
+    {
+        public PostUpdateDtoValidator()
+        {
+            When(p => p.Title != null, () =>
+            {
+                RuleFor(p => p.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(255).WithMessage("Title cannot exceed 255 characters.");
+            });
+
+
+            When(p => p.Slug != null, () =>
+            {
+                RuleFor(p => p.Slug)
+                .NotEmpty().WithMessage("Slug is required.")
+                .Matches("^[a-z0-9]+(-[a-z0-9]+)*$").WithMessage("Slug must be lowercase and words separated by dashes (e.g., 'my-first-post').")
+                .MaximumLength(255).WithMessage("Slug cannot exceed 255 characters.");
+            });
+
+
+            When(p => p.Body != null, () =>
+            {
+                RuleFor(p => p.Body)
+                .NotEmpty().WithMessage("Body is required.")
+                .MinimumLength(50).WithMessage("Body should be at least 50 characters long.");
+            });
+
+
+            When(p => p.Status != null, () =>
+            {
+                RuleFor(p => p.Status)
+                .Must(s => s.HasValue && Enum.IsDefined(typeof(PostStatus), s.Value))
+                .WithMessage("Status must be either Draft or Published.");
+            });
+
+
+            When(p => p.Categories != null, () =>
+            {
+                RuleForEach(p => p.Categories)
+                    .NotEmpty().WithMessage("Category name cannot be empty.")
+                    .MaximumLength(100).WithMessage("Category name cannot exceed 100 characters.");
+            });
+
+
+            When(p => p.Tags != null, () =>
+            {
+                RuleForEach(p => p.Tags)
+                    .NotEmpty().WithMessage("Tag name cannot be empty.")
+                    .MaximumLength(50).WithMessage("Tag name cannot exceed 50 characters.");
+            });
+        }
+
+
+
+    }
+}
