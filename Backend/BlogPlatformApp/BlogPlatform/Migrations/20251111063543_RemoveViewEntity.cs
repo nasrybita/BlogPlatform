e69fddba7b450192c05b5e9f5c5d331d216d0d58
bita <nasrybita@gmail.com>
2025-11-13 19:35:02 +0330

    Add migration: Remove View Entity

---
+++ Backend/BlogPlatformApp/BlogPlatform/Migrations/20251111063543_RemoveViewEntity.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveViewEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Views");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ViewCount",
+                table: "Posts",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "ViewCount",
+                table: "Posts",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldDefaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Views",
+                columns: table => new
+                {
+                    ViewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    ViewCount = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Views", x => x.ViewId);
+                    table.ForeignKey(
+                        name: "FK_Views_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Views_PostId",
+                table: "Views",
+                column: "PostId",
+                unique: true);
+        }
+    }
+}
