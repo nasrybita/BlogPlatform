4c055ea7f2bd73e57d12e1abade72f8efc370308
bita <nasrybita@gmail.com>
2025-12-09 15:34:40 +0330

    Add draft post controller

---
+++ Backend/BlogPlatformApp/BlogPlatform/Controllers/DraftPostController.cs
@@ -0,0 +1,24 @@
+﻿using BlogPlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DraftPostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public DraftPostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDrafts()
+        {
+            var draftPosts = await _postService.GetAllDraftsAsync();
+            return Ok(draftPosts);
+        }
+    }
+}
